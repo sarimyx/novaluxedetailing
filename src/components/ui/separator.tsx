6ba02146492276eb2fa23478f29371cd1a3e0ec8
Sarim <sarim.xs@gmail.com>
2025-03-24 16:22:30 -0600

    Add component

---
+++ src/components/ui/separator.tsx
@@ -0,0 +1,9 @@
+export const Separator = () => {
+    return (
+        <div className="flex items-center justify-center my-8 gap-4 transition-all duration-500 hover:scale-105">
+            <div className="flex-grow h-px bg-yellow-500" />
+            <span className="text-yellow-500 text-xl">✦</span>
+            <div className="flex-grow h-px bg-yellow-500" />
+        </div>
+    )
+}

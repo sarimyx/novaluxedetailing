4156568cafced925eb32db873f6f48a170770818
Sarim <sarim.xs@gmail.com>
2025-03-24 17:05:55 -0600

    Add fonts

---
+++ src/constants/fonts.ts
@@ -0,0 +1,7 @@
+import { Charis_SIL } from "next/font/google";
+
+const Charis_SILFont = Charis_SIL({ subsets: ["latin"], weight: ["400"] });
+
+export const Fonts = {
+  premium: Charis_SILFont,
+};

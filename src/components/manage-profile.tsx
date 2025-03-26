0f617c6ce7334b503cbd4b6722e381944fd9cebe
Sarim <sarim.xs@gmail.com>
2025-03-25 23:22:59 -0600

    Remove sideHidebar

--- src/components/manage-profile.tsx
+++ src/components/manage-profile.tsx
@@ -4,7 +4,7 @@ import { dark } from "@clerk/themes";
 
 export default function ManageAccount() {
   return (
-    <ContentLayout title="Manage Profile" hideSidebar>
+    <ContentLayout title="Manage Profile">
       <section className="flex flex-col space-y-4">
         <span className="text-4xl font-light">Manage profile</span>
         <UserProfile appearance={{ baseTheme: dark }} />

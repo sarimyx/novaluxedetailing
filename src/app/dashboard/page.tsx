6f534392697b1e146b952b5e840c138e826130a3
Sarim <sarim.xs@gmail.com>
2025-03-25 23:33:11 -0600

    Direct page reference

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,15 +1,8 @@
-import { ContentLayout } from "@/components/admin-panel/content-layout";
 import { currentUser } from "@clerk/nextjs/server";
 import { RedirectToSignIn } from "@clerk/nextjs";
 import SelectOrganizationPage from "../select-organization/page";
 
 export default async function DashboardPage() {
   const user = await currentUser();
-  return !user ? (
-    <RedirectToSignIn />
-  ) : (
-    <ContentLayout title="Dashboard">
-      <SelectOrganizationPage />
-    </ContentLayout>
-  );
+  return !user ? <RedirectToSignIn /> : <SelectOrganizationPage />;
 }

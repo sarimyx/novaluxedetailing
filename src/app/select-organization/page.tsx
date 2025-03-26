52275f3772bcb4f991f758e9ce8176aecef7c10c
Sarim <sarim.xs@gmail.com>
2025-03-25 23:33:50 -0600

    Wrap in ContentLayout

--- src/app/select-organization/page.tsx
+++ src/app/select-organization/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { ContentLayout } from "@/components/admin-panel/content-layout";
 import { Styling } from "@/constants/styling";
 import {
   OrganizationList,
@@ -11,14 +12,14 @@ import { dark } from "@clerk/themes";
 
 export default function SelectOrganizationPage() {
   return (
-    <>
+    <ContentLayout title="Select Organization" hideSidebar>
       <SignedIn>
         <div className="flex justify-center px-4 py-8">
           <div className="flex flex-col space-y-4">
             <span
               className={`max-w-80 text-5xl font-light ${Styling.GoldChromatic}`}
             >
-              Select your organization.
+              Select an organization.
             </span>
             <OrganizationList
               appearance={{ baseTheme: dark }}
@@ -33,6 +34,6 @@ export default function SelectOrganizationPage() {
       <SignedOut>
         <RedirectToSignIn />
       </SignedOut>
-    </>
+    </ContentLayout>
   );
 }

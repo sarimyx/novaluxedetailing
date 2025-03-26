7af9ebecc849ad31592e6ec52e758b7d582e2d2f
Sarim <sarim.xs@gmail.com>
2025-03-25 21:46:33 -0600

    Add default font

--- src/constants/fonts.ts
+++ src/constants/fonts.ts
@@ -1,7 +1,9 @@
+import { GeistSans } from "geist/font/sans";
 import { Charis_SIL } from "next/font/google";
 
 const Charis_SILFont = Charis_SIL({ subsets: ["latin"], weight: ["400"] });
 
 export const Fonts = {
+  default: GeistSans,
   premium: Charis_SILFont,
 };

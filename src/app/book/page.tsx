13d1eb61f234ed61e3549da74f6b719e1c1e204d
Sarim <sarim.xs@gmail.com>
2025-05-02 17:54:46 -0600

    Include footer

--- src/app/book/page.tsx
+++ src/app/book/page.tsx
@@ -1,10 +1,12 @@
 import { ContentLayout } from "@/components/admin-panel/content-layout";
+import Footer from "@/components/footer";
 import PackagesComponent from "@/components/packages/packages-component";
 
 export default function Page() {
   return (
     <ContentLayout title="Book" hideSidebar>
       <PackagesComponent />
+      <Footer />
     </ContentLayout>
   );
 }

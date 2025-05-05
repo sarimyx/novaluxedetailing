56fabfc5ce7ae7b80243b71efab00a4789b0518b
Sarim <sarim.xs@gmail.com>
2025-05-04 20:38:10 -0600

    Custom metadata on /book

--- src/app/book/page.tsx
+++ src/app/book/page.tsx
@@ -3,6 +3,14 @@ import { Separator } from "@/components/ui/separator";
 import PackagesComponent from "@/components/packages/packages-component";
 import ReviewsComponent from "@/components/reviews/page";
 import Footer from "@/components/footer";
+import { SiteMetadata } from "@/constants/metadata";
+
+export const metadata = {
+  ...SiteMetadata,
+  title: `Book Your Detail | Nova Luxe Detailing | Utah's Premier Mobile Detailing`,
+  description:
+    "We offer full interior, exterior, and ceramic coating options. Professional service that comes to you.",
+};
 
 export default function Page() {
   return (

1aa2fb939a9afae293eccf4ab00ee65daf9f146c
Sarim <sarim.xs@gmail.com>
2025-05-04 20:17:59 -0600

    Display reviews

--- src/app/book/page.tsx
+++ src/app/book/page.tsx
@@ -1,11 +1,16 @@
 import { ContentLayout } from "@/components/admin-panel/content-layout";
-import Footer from "@/components/footer";
+import { Separator } from "@/components/ui/separator";
 import PackagesComponent from "@/components/packages/packages-component";
+import ReviewsComponent from "@/components/reviews/page";
+import Footer from "@/components/footer";
 
 export default function Page() {
   return (
     <ContentLayout title="Book" hideSidebar noHorizontalPadding>
       <PackagesComponent />
+      <Separator />
+      <ReviewsComponent />
+      <Separator />
       <Footer />
     </ContentLayout>
   );
